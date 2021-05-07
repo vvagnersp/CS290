2f1c4570c9cbe6e016e0a521bba078d2ff0ae88e
spencerwagner0@gmail.com <71741236+vvagnersp@users.noreply.github.com>
2021-05-07 15:07:39 -0700

    Completed activity 5

--- Week 6/Activity 5/testRes.js
+++
@@ -1,4 +0,0 @@
-var req = new XMLHttpRequest();
-req.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=Corvallis, us&appid=ef9275502b390b553029a158a75e738a", false);
-req.send(null);
-console.log(JSON.parse(req.responseText));
