8d2ab35075004213b75b39f8bc0ead507fee62a7
spencerwagner0@gmail.com <71741236+vvagnersp@users.noreply.github.com>
2021-05-07 13:59:02 -0700

    Activity 5 update (complete??)

--- Week 6/Activity 5/script.js
+++ Week 6/Activity 5/script.js
@@ -1,12 +1,13 @@
 // Two requests - openweathermap (GET) and HTTPbin (POST)
 
-// Bind the specified button of getWeather on DOM load
+// Bind the buttons to functions on DOM load
 document.addEventListener("DOMContentLoaded", getWeather);
+document.addEventListener("DOMContentLoaded", httpBin);
 
 // Request weather from openweathermap 
 var apiKey = "ef9275502b390b553029a158a75e738a"; // Unique API key
 function getWeather(){
-  document.getElementById("locSubmit").addEventListener('click', function(event){
+  document.getElementById("locSubmit").addEventListener("click", function(){
     var req = new XMLHttpRequest();
     var loc = document.getElementById("location").value;
 
@@ -23,5 +24,29 @@ function getWeather(){
     newDiv.appendChild(newPar)
      
     document.getElementById("form1").appendChild(newDiv);
+  });
+}
+
+
+// Httpbin POST request
+function httpBin(){
+  document.getElementById("httpBinReq").addEventListener("click", function(){
+    var req = new XMLHttpRequest();
+    
+    req.onreadystatechange = function() {
+      if (this.readyState == XMLHttpRequest.DONE){
+        if (this.status == 0 || (this.status >= 200 && this.status < 400)){
+          // var res = JSON.parse(this.responseText);
+          console.log(this.responseText);
+        } else {
+
+        }
+      }      
+    }
+
+    req.open("POST", "http://httpbin.org/post", true);
+    req.setRequestHeader("Content-type", "application/json");
+    req.send();
+
   });
 }
